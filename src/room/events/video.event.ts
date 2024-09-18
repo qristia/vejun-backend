be631dfae14794e880bf3ac5918930b46e22f7b1
Christian Sena <csenat90@gmail.com>
2024-09-18 20:45:18 -0300

    add more event types

--- src/room/events/video.event.ts
+++ src/room/events/video.event.ts
@@ -2,6 +2,11 @@ import { BaseRoomEvent } from './base.event';
 
 export type VideoEvent = BaseRoomEvent;
 
+export interface VideoPlayingChangeEvent extends BaseRoomEvent {
+  time: number;
+  state: boolean;
+}
+
 export interface VideoUpdateEvent extends BaseRoomEvent {
   time: number;
   isPlaying: boolean;
